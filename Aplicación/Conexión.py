ed4e3de4fea64cb9467b5656d4c8e309472183c5
José Julián Brenes Garro <jo.brenes@estudiantec.cr>
2024-11-22 22:47:45 -0600

    pantalla inicio en html

--- Aplicación/Conexión.py
+++ Aplicación/Conexión.py
@@ -1,18 +1,70 @@
 import pyodbc
 
-try:
-    connection = pyodbc.connect('DRIVER={SQL Server};SERVER=WINDOWS-0GERP4M;DATABASE=Ecommerce;UID=user;PWD=1234')
-    print("Conexión exitosa.")
-    cursor = connection.cursor()
-    cursor.execute("SELECT @@version;")
-    row = cursor.fetchone()
-    print("Versión del servidor de SQL Server: {}".format(row))
-    cursor.execute("SELECT * FROM Marcas")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-except Exception as ex:
-    print("Error durante la conexión: {}".format(ex))
-finally:
-    connection.close()  # Se cerró la conexión a la BD.
-    print("La conexión ha finalizado.")
+def prueba():
+    try:
+        connection = pyodbc.connect('DRIVER={SQL Server};SERVER=WINDOWS-0GERP4M;DATABASE=Ecommerce;UID=user;PWD=1234')
+        print("Conexión exitosa.")
+        cursor = connection.cursor()
+        cursor.execute("SELECT @@version;")
+        row = cursor.fetchone()
+        print("Versión del servidor de SQL Server: {}".format(row))
+        cursor.execute("SELECT * FROM Marcas")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+    except Exception as ex:
+        print("Error durante la conexión: {}".format(ex))
+    finally:
+        connection.close()  # Se cerró la conexión a la BD.
+        print("La conexión ha finalizado.")
+
+def getTop5Categorias():
+    try:
+        connection = pyodbc.connect('DRIVER={SQL Server};SERVER=WINDOWS-0GERP4M;DATABASE=Ecommerce;UID=user;PWD=1234')
+        cursor = connection.cursor()
+        cursor.execute("EXEC ObtenerTop5Categorias")
+        rows = cursor.fetchall()
+        lista=[]
+        for row in rows:
+           lista.append(row[0])
+        return lista
+    except Exception as ex:
+        print("Error durante la conexión: {}".format(ex))
+    finally:
+        connection.close()
+
+def getPromocionesInicio():
+    try:
+        connection = pyodbc.connect('DRIVER={SQL Server};SERVER=WINDOWS-0GERP4M;DATABASE=Ecommerce;UID=user;PWD=1234')
+        cursor = connection.cursor()
+        cursor.execute("EXEC ObtenerPromocionInicio")
+        rows = cursor.fetchall()
+        lista=[]
+        for row in rows:
+            lista.append({
+                'nombre': row[0],
+                'precio': float(row[1]),  # Convertir Decimal a float
+                'descuento': float(row[2]),  # Convertir Decimal a float
+                'fecha': row[3].strftime('%d/%m/%Y'),  # Formatear fecha
+                'imagen': row[4]
+            })
+        return lista
+    except Exception as ex:
+        print("Error durante la conexión: {}".format(ex))
+    finally:
+        connection.close()
+
+def validarUsuario(correo,contrasena):
+    try:
+        connection = pyodbc.connect('DRIVER={SQL Server};SERVER=WINDOWS-0GERP4M;DATABASE=Ecommerce;UID=user;PWD=1234')
+        cursor = connection.cursor()
+        cursor.execute("EXEC ValidarUsuario @correo_electronico = ?, @contrasena = ?", correo, contrasena)
+        result = cursor.fetchone()
+        if result:
+            return result
+        else:
+            return False
+    except Exception as ex:
+        print("Error durante la conexión: {}".format(ex))
+    finally:
+        connection.close()
--- Aplicación/app.py
+++ Aplicación/app.py
@@ -1,23 +1,37 @@
 from flask import Flask, render_template, request
+from Conexión import *
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='Productos')
 
 # Ruta para la página principal
 @app.route('/')
 def home():
-    return render_template('index.html')
+    categorias = getTop5Categorias()
+    promociones = getPromocionesInicio()
+    return render_template('index.html', 
+                           categorias=categorias,
+                           promociones=promociones)
 
 # Ruta para procesar datos del formulario (opcional)
-@app.route('/accion1', methods=['POST'])
-def accion1():
-    dato = request.form.get('dato')
-    return f"Acción 1 ejecutada con: {dato}"
+@app.route('/inicio', methods=['POST'])
+def inicio():
+    correo = request.form.get('correo')
+    contrasena= request.form.get('contraseña')
+    if  validarUsuario(correo,contrasena) != False:
+        return f"Acción 1 ejecutada con: {correo, contrasena}"
+    else:
+        categorias = getTop5Categorias()
+        promociones = getPromocionesInicio()
+        return render_template('index.html',
+                                mensaje="Usuario no registrado o credenciales incorrectas.",
+                                categorias=categorias,
+                                promociones=promociones)
 
 # Ruta para manejar acción 2
-@app.route('/accion2', methods=['POST'])
-def accion2():
-    otro_dato = request.form.get('otro_dato')
-    return f"Acción 2 ejecutada con: {otro_dato}"
+#@app.route('/accion2', methods=['POST'])
+#def accion2():
+#    otro_dato = request.form.get('otro_dato')
+#    return f"Acción 2 ejecutada con: {otro_dato}"
 
 
 if __name__ == '__main__':
